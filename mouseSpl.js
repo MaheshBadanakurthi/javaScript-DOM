ea5af9363846bf89de8fbb8a8c0801224f4b506a
Mahesh <itstribe.mahi@gmail.com>
2022-08-19 18:54:08 +0530

    used Onload and Onfocus Properties

---
+++ mouseSpl.js
@@ -0,0 +1,21 @@
+
+        function old(){    // for alert used onload
+            alert("you are going to perform some actions now")
+
+        }
+        function ofs(t){   // for color change onfocus for for input fields
+            t.style.color="red";
+            t.style.backgroundColor="black";
+        }
+        function ofs2(j){
+            j.style.color="navy";
+            j.style.backgroundColor="pink";
+        }
+        function ofs4(tk){
+            tk.style.color="green";
+            tk.style.backgroundColor="navy";
+        }
+        function ofs6(tl){
+            tl.style.color="pink";
+            tl.style.backgroundColor="yellow";
+        }
