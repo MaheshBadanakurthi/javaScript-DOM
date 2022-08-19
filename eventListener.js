0ae27c99f50771f3f35a3be05fde26f13136fd50
Mahesh <itstribe.mahi@gmail.com>
2022-08-19 20:25:52 +0530

    added event Listner and alert msg

---
+++ eventListener.js
@@ -0,0 +1,25 @@
+document.getElementById("toChangeHead").style.backgroundColor="navy";
+document.getElementById("eventFunction").addEventListener("click", blockAChange);
+function blockAChange(){
+    document.getElementById("blockA").style.backgroundColor="green";
+    document.getElementById("blockA").style.color="white";
+
+}
+document.getElementById("eventFunction").addEventListener("click", blockBChange);
+
+function blockBChange(){
+    document.getElementById("blockB").style.backgroundColor="red";
+    document.getElementById("blockB").style.color="white";
+    alert("you are going to see som echanges")
+
+}
+
+document.getElementById("eventFunction").addEventListener("click", DateCome);
+
+function DateCome(){
+    document.getElementById("forDate").innerHTML=Date();
+    alert("You are going to see Date now")
+
+
+}
+
