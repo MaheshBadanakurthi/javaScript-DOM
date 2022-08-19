a0ee0a46927d7b187470270a00f66abcec636185
Mahesh <itstribe.mahi@gmail.com>
2022-08-19 21:21:33 +0530

    mouse hover, out, down properties

---
+++ mouseProp.js
@@ -0,0 +1,28 @@
+
+
+        function mOver(k){
+            k.innerHTML="thank you"
+            document.getElementById("p9").style.color="navy";
+        }
+        function mOut(s){
+            // let x=document.getElementById("p9")black
+            s.style.backgroundColor="black";
+            s.style.color="green";
+            s.innerHTML="bring mouse on me"
+            document.getElementById("p9").style.color="navy";
+        }
+        function mDn(g){
+            // down means you presed
+            g.innerHTML="You down the mouse"
+            // document.getElementById("p9").style.color="navy";
+        }
+        function oMw(l){
+            // mouse wheel action 
+            l.innerHTML=""
+            document.getElementById("p9").style.color="navy";
+        }
+        function act(){
+            alert("carefull u r going to perform some actions")
+        }
+
+
