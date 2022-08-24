45990cf0bfa0136118c297f18ac3d596a6d90486
Mahesh <itstribe.mahi@gmail.com>
2022-08-24 17:05:15 +0530

    DOM functions

---
+++ new.js
@@ -0,0 +1,4 @@
+function click(){
+   let x= document.getElementById("image");
+   x.src="smile.jpeg";
+}
---
+++ script.js
@@ -0,0 +1,17 @@
+// get element by id
+document.getElementById('qut').style.border="1px solid green"
+
+
+document.getElementById("a1").style.color="red";
+document.getElementById("qut").style.color = "blue";
+
+function cFun(){
+    document.getElementById("u7").src="cheetah.jpg";
+    // document.getElementById("u7").appendChild('<img src="cheetah.jpg" >')
+    
+}
+
+
+
+
+
